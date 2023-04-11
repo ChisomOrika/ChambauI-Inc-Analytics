418717c5f240a22afdd19287541747f84bd55f03
Orika Chisom <chisomorika@gmail.com>
2023-04-11 13:23:20 +0100

    DB crendentials

---
+++ config.py
@@ -0,0 +1,8 @@
+# database credentials
+db_user = 'chisorik5367'
+db_password = 'IGsPcgU73Y'
+db_host = 'd2b-internal-assessment-dwh.cxeuj0ektqdz.eu-central-1.rds.amazonaws.com'
+db_port = 5432
+db_name = 'd2b_assessment'
+staging_schema_name = 'chisorik5367_staging'
+analytics_schema_name = 'chisorik5367_analytics'
